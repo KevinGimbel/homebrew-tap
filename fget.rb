6205ee2cb27231ae7c12d2a55cfeeb430e6ab62b
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-12 17:41:35 +0200

    fget version v1.0.1

--- fget.rb
+++ fget.rb
@@ -1,12 +1,12 @@
 class Fget < Formula
   desc "Download files with go, similar to wget."
   homepage "https://github.com/kevingimbel/fget"
-  url "https://github.com/kevingimbel/fget/releases/download/v1.0.0/fget_1.0.0_macOS_64-bit.tar.gz"
-  version "1.0.0"
-  sha256 "6e78a904cf7b03861fefce4d9dc502e40a1953658dcbe89a05a486ac248e1742"
+  url "https://github.com/kevingimbel/fget/releases/download/v1.0.1/fget_1.0.1_macOS_64-bit.tar.gz"
+  version "1.0.1"
+  sha256 "f53f27e09e4ce9ecee7c82af99bf0b97f863adb5e1069905b90b4bd1cef204ef"
 
   def install
-    bin.install "goserve"
+    bin.install "fget"
   end
 
   test do
