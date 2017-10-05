3edc714f3b41c851a6453d443f446af9f9d643a4
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-05 13:35:54 +0200

    goserve version v1.1.0

--- goserve.rb
+++ goserve.rb
@@ -1,9 +1,9 @@
 class Goserve < Formula
   desc "Simple static file server written in Go"
   homepage "https://github.com/kevingimbel/goserve"
-  url "https://github.com/kevingimbel/goserve/releases/download/v1.0.2/goserve_1.0.2_macOS_64-bit.tar.gz"
-  version "1.0.2"
-  sha256 "1c0c7ef38c070210b1903443037b4352218e017c2e439b6a8297bf0ddc95f0e8"
+  url "https://github.com/kevingimbel/goserve/releases/download/v1.1.0/goserve_1.1.0_macOS_64-bit.tar.gz"
+  version "1.1.0"
+  sha256 "92a724d5e4f62cf634046cbf8f2aa8d2d08b44b5b02a3080f2d73036550398be"
 
   def install
     bin.install "goserve"
