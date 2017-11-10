2e8d90956c98ac7db21ba2c304a31c407bbcf712
goreleaserbot <goreleaser@carlosbecker.com>
2017-11-10 10:18:28 +0100

    license version v1.0.2

--- license.rb
+++ license.rb
@@ -1,9 +1,9 @@
 class License < Formula
   desc "CLI tool to download license files."
   homepage "https://github.com/kevingimbel/license"
-  url "https://github.com/kevingimbel/license/releases/download/v1.0.1/license_1.0.1_macOS-64bit.tar.gz"
-  version "1.0.1"
-  sha256 "0d582d066d65bdce54f39cbe780ffac0807bb490077a20d8fda6379bf70fa877"
+  url "https://github.com/kevingimbel/license/releases/download/v1.0.2/license_1.0.2_macOS-64bit.tar.gz"
+  version "1.0.2"
+  sha256 "f14e620293e35450687764b63497b4311edbe6fd834378d51c35f11d291589ac"
 
   def install
     bin.install "license"
