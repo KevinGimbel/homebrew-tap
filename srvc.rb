f458d6ef43dbf0dda4011b4cb5641c864ec02afc
goreleaserbot <goreleaser@carlosbecker.com>
2018-01-23 13:59:11 +0100

    srvc version v1.0.1

--- srvc.rb
+++ srvc.rb
@@ -1,9 +1,9 @@
 class Srvc < Formula
   desc "Quick API prototyping tool"
   homepage "https://github.com/kevingimbel/srvc"
-  url "https://github.com/kevingimbel/srvc/releases/download/v1.0.0/srvc_1.0.0_macOS_64-bit.tar.gz"
-  version "1.0.0"
-  sha256 "680b0cd7dbec73d1848ea8e2f2c7f7a24736656d53dcc1c6f27aa545aa4c2e65"
+  url "https://github.com/kevingimbel/srvc/releases/download/v1.0.1/srvc_1.0.1_macOS_64-bit.tar.gz"
+  version "1.0.1"
+  sha256 "43802a33e76cbe6bfb734c74b63c27089574d7959fae7785ac9d1015799e5966"
 
   def install
     bin.install "srvc"
